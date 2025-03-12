b2802de59700921540857928e7310017d9312087
Mohanraj A <146074793+A-MOHANRAJ-07@users.noreply.github.com>
2025-03-12 14:36:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def upload_image():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=8080)
+    app.run(debug=True,host="0.0.0.0",port=10000)
